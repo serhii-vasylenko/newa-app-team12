b39c02cfeac0bb68637c7834d58ba0bb71ceb5dd
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-02-17 19:27:40 +0200

    created startup files

---
+++ src/js/pagination.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const key = 'XINA7yqi4ZNcq46o0RcxxTCJymTVrKDJ';
+
+function fetchPopularNews() {
+  return axios
+    .get(
+      `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${key}`
+    )
+    .then(response => response.data);
+}
+
+// function fetchNewsBySearchValue() {
+//   return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=ukraine&api-key=${key}`).then(response => response.data);
+// }
+
+fetchPopularNews()
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
