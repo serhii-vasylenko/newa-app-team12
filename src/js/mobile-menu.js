363cae6e529e3dd06b9f9a3a8f6a7b5e8da92865
Yevhenii009 <stifgun@gmail.com>
2023-02-17 23:50:27 +0200

    комент больіне-меню2

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -19,30 +19,13 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenu = document.querySelector('.js-open-menu');
 const closeMenu = document.querySelector('.js-close-menu');
-
-// const bodyScrollLock = require('body-scroll-lock');
-// import debounce from 'lodash.debounce';
+const themeContainer = document.querySelector('.theme');
 
 const toggleMenu = () => {
-  const isMenuOpen =
-  openMenu.getAttribute('aria-expanded') === 'true' || false;
-  openMenu.setAttribute('aria-expanded', !isMenuOpen);
   mobileMenu.classList.toggle('is-open');
-
   if (themeContainer.classList.contains('mobile')) {
     themeContainer.classList.remove('mobile');
   } else if (!themeContainer.classList.contains('mobile')) {
@@ -52,24 +35,8 @@ const toggleMenu = () => {
 
     debouncedThemeContainer();
   }
-
-  const scrollLockMethod = !isMenuOpen
-    ? 'disableBodyScroll'
-    : 'enableBodyScroll';
-
-  bodyScrollLock[scrollLockMethod](document.body);
 };
 
 openMenu.addEventListener('click', toggleMenu);
 closeMenu.addEventListener('click', toggleMenu);
 
-window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  if (!e.matches) return;
-  mobileMenu.classList.remove('is-open');
-  openMenu.setAttribute('aria-expanded', false);
-  bodyScrollLock.enableBodyScroll(document.body);
-});
-
-// if (window.innerWidth < 768) {
-//   seachBtn.setAttribute('type', 'button');
-// }
