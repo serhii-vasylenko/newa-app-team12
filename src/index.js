86f818528519708d91660558be5340b09f626548
Kvazar7 <Kometa737@gmail.com>
2023-02-19 08:57:46 +0200

    feature/open-close-read
    створив кнопки відкривання/закривання прочитаних новин зі стилями та функцією.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './js/mobile-menu';
-import './js/categories';
+// import './js/mobile-menu';
+// import './js/categories';
 import './js/getPopoularProduct';
 import './js/getSearchNews';
 import './js/pagination';
 // import './js/weather';
 // import './js/api/weather-api';
 // import './js/markups/weather-markup';
+import './js/openCloseRevisionDate';
