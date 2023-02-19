3c41107afbf0fba90418f3cf4e464cf57adac664
Daniil Popov <x6uhrox@gmail.com>
2023-02-19 17:40:38 +0200

    Add change

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,14 +1,20 @@
 import { getCategoryNews } from './getCategoryNews.js';
-import { markupCategories } from './markups/markupCategoriesNews.js';
+import {
+  markupCategories,
+  markupNameButton,
+} from './markups/markupCategoriesNews.js';
 //import function get Date from Calendar
 
 const categoriesMenu = document.querySelector('.category__menu');
-const categoriesBtn = document.querySelector('.category__btn');
+const categoriesBtn = document.querySelector('#btn-open-category');
+const categoriesList = document.querySelector('.buttons-list');
 
 markupCategories(categoriesMenu);
+markupNameButton();
 
 categoriesBtn.addEventListener('click', onToggleCategoriesMenu);
 categoriesMenu.addEventListener('click', onSearchNews);
+categoriesList.addEventListener('click', onSearchNews);
 
 function onToggleCategoriesMenu() {
   const isMenuOpen =
@@ -17,11 +23,11 @@ function onToggleCategoriesMenu() {
   categoriesMenu.classList.toggle('is-open-categories');
 }
 
-window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  if (!e.matches) return;
-  categoriesMenu.classList.remove('is-open-categories');
-  categoriesBtn.setAttribute('aria-expanded', false);
-});
+// window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//   if (!e.matches) return;
+//   categoriesMenu.classList.remove('is-open-categories');
+//   categoriesBtn.setAttribute('aria-expanded', false);
+// });
 
 window.onclick = event => {
   if (!event.target.matches('.category__btn')) {
@@ -37,3 +43,18 @@ function onSearchNews(event) {
   const currentButtonCategory = event.target.dataset.btn;
   getCategoryNews(currentButtonCategory);
 }
+
+export function divideArrNews(categories, count) {
+  const categoriesArrs = [];
+  categoriesArrs.push(categories.slice(0, count));
+  categoriesArrs.push(categories.slice(count));
+  return categoriesArrs;
+}
+
+window.addEventListener('resize', () => {
+  markupNameButton();
+  markupCategories(categoriesMenu);
+  categoriesMenu.classList.remove('is-open-categories');
+  categoriesBtn.setAttribute('aria-expanded', false);
+});
+
