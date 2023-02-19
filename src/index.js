c916a25e6524e8e447ede0349f9ebeef95c43e54
OleksandraSt <communication1@ukr.net>
2023-02-19 12:22:16 +0200

    додає зміни до header

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './js/categories';
 import './js/getPopoularProduct';
 import './js/getSearchNews';
 import './js/pagination';
+import './js/switch';
 // import './js/weather';
 // import './js/api/weather-api';
 // import './js/markups/weather-markup';
