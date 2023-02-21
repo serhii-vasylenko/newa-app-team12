c340bb66578b93a08b8f87bdb50b8ec233302e39
vikiv5 <vi_kov@ukr.net>
2023-02-21 15:52:20 +0200

    search news

--- src/js/searchnews.js
+++ src/js/searchnews.js
@@ -1,58 +1,169 @@
-import { getSearchNewsAPI } from './api/news-api.js';
+import SearchNews from "./api/api-newssearch";
 
 const newsGallery = document.querySelector('.news-gallery');
 
-//const newsApi = new NewsApi();
+const pageNotFound = document.querySelector(".not-found");
+const input = document.querySelector('.search-form__input');
+input.addEventListener('submit', onEnterPush);
+
+
+const searchNews = new SearchNews();
+
+function onEnterPush (e) {
+  e.preventDefault () 
+  const input = e.currentTarget ;
+  searchNews.searchQuery=input.elements.searchQuery.value.trim();
+  clearImgList ();
+  searchNews.resetPage();
+  searchNewsImg();
+}
+
+async function searchNewsImg() {
+  try {
+  const getNews = await searchNews.searchNewsImg();
+  console.log('Arr objects with search News ', getNews.results);
+  //const resArr = getNews.results;
+  newsGallery.innerHTML = addMarkup()
+
+if (getNews.length) {
+  pageNotFound.classList.add("visually-hidden");
+  addMarkup(getNews);
+} else if (getNews.length === 0) {
+  notFound();
+}
+}
+catch (err){
+  notFound(err)}
+}
+
+function addMarkup(data) {
+  return data.map(({ abstract, media, published_date, subsection, title, url }) => {
+
+      let mediaURL = "https://amsrus.ru/wp-content/uploads/2016/02/Mercedes-Benz-C63-AMG-Black-Series-1.jpg";
+
+      if (media && media[0] && media[0]["media-metadata"] && media[0]["media-metadata"][2]) {
+          mediaURL = media[0]["media-metadata"][2].url;
+        }
+  return `
+  <li class="card-news__item card__readed">
+  <div class="card-news__picture"><img src="${mediaURL}" alt="${media[0]?.caption}" class="news-image">
+    <p class="news-category"> ${subsection}</p>
+    <button class="news-favorite" aria-label="add to favorite">Add to favorite <svg class="news-favorite__icon" width="16" height="16"><use href="../images/icons-defs.svg#icon-heart-transparent"></use></svg>
+    </button>
+  </div>
+  <div class="card-news__info">
+    <h3 class="card-news__title"> ${title}</h3>
+    <p class="card-news__info"> ${abstract}...</p>
+    <div class="card-information">
+      <div class="card-infrmation__data"> ${published_date}</div>
+      <a class="card__infotion__more" href="${url}">Read more</a>
+    </div>
+  </div>
+  <div class="owerlay-readed is-hidden">
+    <p class="owerlay-readed__info" aria-label="readed">Already read <svg class="owerlay-readed__icon" width="18"  height="18"><use href="../images/icons-defs.svg#icon-readed"></use></svg></p>
+  </div>
+</li>`}).join('')
+
+}
+
+
+
+
+function notFound() {
+  if (pageNotFound.classList.contains("visually-hidden")) {
+    newsGallery.innerHTML = " ";
+    pageNotFound.classList.remove("visually-hidden");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { getSearchNewsAPI } from './api/news-api.js';
+
+const newsGallery = document.querySelector('.news-gallery');
 
 const pageNotFound = document.querySelector(".not-found");
-
 const input = document.querySelector('.search-form__input');
-
 input.addEventListener('submit', onEnterPush);
 
 function onEnterPush(e) {
     const query = e.target.value;
-  
-    console.log(query);
     getSearchNews(query);
   }
 
   async function getSearchNews() {
+    try {
     const getNews = await getSearchNewsAPI();
-    console.log('Arr objects with search News ', getNews.response.docs);
-    
-  }
-
-  //const articles = await response.json();
-  const resArr = getNews.response.docs;
+    console.log('Arr objects with search News ', getNews.results);
+    const resArr = getNews.results;
+    newsGallery.innerHTML = addMarkup(resArr)
 
   if (resArr.length) {
-    pageNotFound.classList.add("is-hidden");
-    arrHandler(resArr);
+    pageNotFound.classList.add("visually-hidden");
+    addMarkup();
   } else if (resArr.length === 0) {
     notFound();
   }
+}
+  catch (err){
+    notFound(err)}
+  }
   // --> section not-found
 
-  console.log(resArr)
-
   function notFound() {
-    if (pageNotFound.classList.contains("is-hidden")) {
-      // newsGallery.innerHTML = " ";
-      pageNotFound.classList.remove("is-hidden");
+    if (pageNotFound.classList.contains("visually-hidden")) {
+      newsGallery.innerHTML = " ";
+      pageNotFound.classList.remove("visually-hidden");
     }
   }
 
- // function arrHandler(arr) {
-    
-    
-   // createBaseMarcup(objArr);
-  //}
   
-  //function createBaseMarcup(arr) {
-    //addMarkup()
+    function addMarkup(data) {
+      return data.map(({ abstract, media, published_date, subsection, title, url }) => {
   
-  function addMarkup(tagString) {
+          let mediaURL = "https://amsrus.ru/wp-content/uploads/2016/02/Mercedes-Benz-C63-AMG-Black-Series-1.jpg";
+  
+          if (media && media[0] && media[0]["media-metadata"] && media[0]["media-metadata"][2]) {
+              mediaURL = media[0]["media-metadata"][2].url;
+            }
+      return `
+      <li class="card-news__item card__readed">
+      <div class="card-news__picture"><img src="${mediaURL}" alt="${media[0]?.caption}" class="news-image">
+        <p class="news-category"> ${subsection}</p>
+        <button class="news-favorite" aria-label="add to favorite">Add to favorite <svg class="news-favorite__icon" width="16" height="16"><use href="../images/icons-defs.svg#icon-heart-transparent"></use></svg>
+        </button>
+      </div>
+      <div class="card-news__info">
+        <h3 class="card-news__title"> ${title}</h3>
+        <p class="card-news__info"> ${abstract}...</p>
+        <div class="card-information">
+          <div class="card-infrmation__data"> ${published_date}</div>
+          <a class="card__infotion__more" href="${url}">Read more</a>
+        </div>
+      </div>
+      <div class="owerlay-readed is-hidden">
+        <p class="owerlay-readed__info" aria-label="readed">Already read <svg class="owerlay-readed__icon" width="18"  height="18"><use href="../images/icons-defs.svg#icon-readed"></use></svg></p>
+      </div>
+    </li>`}).join('')
+    
+  }
+  
+  
+ /* function addMarkup(tagString) {
   newsGallery.innerHTML = '';
   newsGallery.insertAdjacentHTML('beforeend', tagString);
-  }
+  }*/
+
+
+
