5a98debdc0879d870f3ef5d0cd44ca872a712e52
serhii-vasylenko <113785494+serhii-vasylenko@users.noreply.github.com>
2023-02-21 19:30:09 +0000

    Deploying to gh-pages from @ serhii-vasylenko/news-app-team12@ac5dfa67341d64a62cef42450dbba156ee5bee2b 🚀

--- read.80c0c069.js
+++
@@ -1,2 +0,0 @@
-let e=document.getElementsByClassName("revision-date-btn");for(let t=0;t<e.length;t++)e[t].addEventListener("click",(function(){this.classList.toggle("active-revision-date");let e=this.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"}));
-//# sourceMappingURL=read.80c0c069.js.map
