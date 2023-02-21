a632ff3aec95d7846c4c28506461d78e1276cdc0
serhii-vasylenko <113785494+serhii-vasylenko@users.noreply.github.com>
2023-02-21 22:36:10 +0200

    Revert "редагування вар змінних та налаштування перемикача теми"

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,18 +1,17 @@
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenu = document.querySelector('.js-open-menu');
 const closeMenu = document.querySelector('.js-close-menu');
-
+// const chekBox = document.querySelector('.theme-switch');
 
 const bodyScrollLock = require('body-scroll-lock');
-
 // import debounce from 'lodash.debounce';
 const toggleMenu = () => {
   const openMobileMenu =
     openMenu.getAttribute('aria-expanded') === 'true' || false;
     openMenu.setAttribute('aria-expanded', !openMobileMenu );
     mobileMenu.classList.toggle('is-open');
-
   
+
   const scrollLockMethod = !openMobileMenu
     ? 'disableBodyScroll'
     : 'enableBodyScroll';
@@ -22,34 +21,18 @@ const toggleMenu = () => {
 
 const title = document.querySelector('meta[name="description"]').content.toLowerCase();
 const links = document.querySelectorAll('.nav-mobile__link');
-const icons = document.querySelectorAll('.mobile-page__icon');
-
-
 
 links.forEach(link => {
   if (link.innerText.toLowerCase() === title) {
     link.classList.add('nav-mobile__link--current');
   }
-  
-
-// icons.forEach(icon => {
-//   if (icon.innerText === title) {
-//     icon.classList.toggle('nav-mobile__link--current');
-  
-//   }   
-// })
+});
 
 
-  });
-  //   icons.forEach(icon => {
-  //   if (!title) {
-  // icon.toggleAttribute(disable)
-  //   }
-  // })
-  
+// const toggleMenu = () => {
+//   mobileMenu.classList.toggle('is-open');
 
-
+// };
 
 openMenu.addEventListener('click', toggleMenu);
 closeMenu.addEventListener('click', toggleMenu);
-
