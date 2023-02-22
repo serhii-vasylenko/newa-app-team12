1b23e67be5ff93dbe3ecfc06c611a3a75bdf58b7
Jerrssey <fankton@gmail.com>
2023-02-22 13:24:22 +0200

    Update categories.js

--- src/js/categories.js
+++ src/js/categories.js
@@ -64,7 +64,7 @@ export function divideArrNews(categories, count) {
 }
 
 function clearCategoriesMenu() {
-  saveMargin();
+  // saveMargin();
   markupNameButton();
   markupCategories(categoriesMenu);
   categoriesMenu.classList.remove('is-open-categories');
