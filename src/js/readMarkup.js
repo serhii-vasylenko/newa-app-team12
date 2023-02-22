84795461cccc2f8139c1cae077b0fb3919bf6129
Kvazar7 <Kometa737@gmail.com>
2023-02-22 11:56:26 +0200

    оновлюю сторінку read

---
+++ src/js/readMarkup.js
@@ -0,0 +1,35 @@
+import { openCloseRevisionDate } from './openCloseRevisionDate';
+
+const revisionDateContainer = document.querySelector('.revision-date-container');
+
+window.addEventListener('DOMContentLoaded', creatRevisionData);
+
+function creatRevisionData() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i)
+      if (key !== 'favorite')
+    
+    revisionDateContainer.insertAdjacentHTML("afterbegin",
+      `<div>
+        <button type="button" class="revision-date-btn">${key}
+          <svg class="icon-arrow">
+            <use href="./images/arrow.svg#icon-arrow"></use>
+          </svg>
+        </button>
+        <ul class="container-for-viewed-cards">
+        </ul>
+        </div>`);
+    
+    const gallery = document.querySelector('.container-for-viewed-cards');
+    try {
+      const storageData = JSON.parse(localStorage.getItem(key));
+      const galleryMarkup = storageData.reduce((previousValue, obj) => {
+        return previousValue + obj.markup;
+      }, '');
+      gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+    } catch {
+      console.log('LocalStorage is empty!');
+    }
+  }
+  openCloseRevisionDate();
+}
