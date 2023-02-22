4160cd3ac1ba0d0f2c8cdb84864930a072db91b6
Jerrssey <fankton@gmail.com>
2023-02-22 13:14:09 +0200

    Revert "Update getPopoularProduct.js"
    This reverts commit c83d5b79f0241483e3da2fcb1cdf129d7c4b35a8.

--- src/js/getPopoularProduct.js
+++ src/js/getPopoularProduct.js
@@ -105,12 +105,7 @@ function markup({ abstract, media, published_date, subsection, title, url }) {
     <li class="card-news__item card__readed">
     <div class="card-news__picture"><img src="${mediaURL}" alt="${media[0]?.caption}" class="news-image">
       <p class="news-category"> ${subsection}</p>
-      <button class="news-favorite" aria-label="add to favorite">Add to favorite <svg class="news-favorite__icon" width="16"
-      height="16"
-      viewBox="0 0 37 32"
-      >
-<path style="stroke: var(--color1, #4440F7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
-</svg>
+      <button class="news-favorite" aria-label="add to favorite">Add to favorite <svg class="news-favorite__icon" width="16" height="16"><use href="../images/icons-defs.svg#icon-heart-transparent"></use></svg>
 
       </button>
     </div>
