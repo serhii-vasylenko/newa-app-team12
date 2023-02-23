c190ce18ea2eaa24cc1ee8523eb093c53591c5b5
vikiv5 <vi_kov@ukr.net>
2023-02-24 00:04:20 +0200

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,36 +1,10 @@
-// const search = document.querySelector('.search-form')
-// const btn = document.querySelector('.search-form__button')
-// const input = document.querySelector('.search-form__input')
+const search = document.querySelector('.search-form');
+const btn = document.querySelector('.search-form__button');
+const input = document.querySelector('.search-form__input');
 
-// btn.addEventListener('click', (e) => {
-//   e.preventDefault()
-  
-//   if(search.classList.contains('active')) {
-//     search.submit()
-//   }
-//   else {
-//     search.classList.toggle('active')
-//     input.focus()  
-//   }
-// })
-
-const search = document.querySelector('.search-form')
-const btn = document.querySelector('.search-form__button')
-const input = document.querySelector('.search-form__input')
-
-/*btn.addEventListener('click', (e) => {
- e.preventDefault()
-  
- if(search.classList.contains('active')) {
-   search.submit()
- }
-  else {
-   search.classList.toggle('active')
-   input.focus()  
- }
-})*/
 search.addEventListener('submit', e => {
   e.preventDefault();
+
   if (e.currentTarget.classList.contains('active')) {
     onEnterPush();
   } else {
