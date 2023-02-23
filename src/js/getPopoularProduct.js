540a47d077ea9c896bb2387a2ce857db6a8e6914
Katerina <Kateryna.e.nikitina@gmail.com>
2023-02-23 21:09:53 +0200

    change minor styles

--- src/js/getPopoularProduct.js
+++ src/js/getPopoularProduct.js
@@ -23,6 +23,7 @@ async function getPopularProduct() {
     const newsArr = getNews.results;
 
     let markupNews = '';
+
     const markupWeather = getMarkupWeather({ data: weatherData });
     // console.log(markupWeather);
     // console.log({ data: weatherData });
@@ -74,6 +75,8 @@ async function getPopularProduct() {
 getPopularProduct();
 
 
+// Функція винесена в окремий файл newsCard.js
+
 // function markup({ abstract, media, published_date, subsection, title, url }) {
 //   let mediaURL =
 //     'https://amsrus.ru/wp-content/uploads/2016/02/Mercedes-Benz-C63-AMG-Black-Series-1.jpg';
@@ -104,55 +107,4 @@ getPopularProduct();
 //       <p class="owerlay-readed__info" aria-label="readed">Already read <svg class="owerlay-readed__icon" width="18"  height="18"><use href="../images/icons-defs.svg#icon-readed"></use></svg></p>
 //     </div>
 //   </li>`;
-// }
-// import { getPopularNewsAPI } from './api/news-api.js';
-// // import {newsTemplate} from '../templates/card-news.hbs';
-// import { checkFavCards } from './addAndRemoveFromFavorite.js';​
-// const popularNewsGallery = document.querySelector('.news-gallery');
-// const notFoundPage = document.querySelector('.not-found');​
-// async function getPopularProduct() {
-//   try {
-//     const getNews = await getPopularNewsAPI();
-//     console.log('Arr objects with mostpopular News', getNews.results);
-//     const newsArr = getNews.results;
-//     popularNewsGallery.innerHTML = markup(newsArr);
-//     checkFavCards();
-//   } catch (error) {
-//     notFoundPage.classList.toggle('visually-hidden');
-//   }
-// }​
-// getPopularProduct();
-// function markup(data) {
-//   return data
-//     .map(({ abstract, media, published_date, subsection, title, url }) => {
-//       let mediaURL = `https://webassets.eurac.edu/31538/1647583511-adobestock_490465800.jpeg?auto=format&fm=jpg&h=588&w=980`;
-//       if (
-//         media &&
-//         media[0] &&
-//         media[0]['media-metadata'] &&
-//         media[0]['media-metadata'][2]
-//       ) {
-//         mediaURL = media[0]['media-metadata'][2].url;
-//       }
-//       return `
-//     <li class="card-news__item">
-//     <div class="card-news__picture"><img src="${mediaURL}" alt="${media[0]?.caption}" class="news-image">
-//       <p class="news-category"> ${subsection}</p>
-//       <button class="news-favorite addToFavoriteBtn" aria-label="add to favorite">Add to favorite <svg class="news-favorite__icon" width="16" height="16"><use href="../images/icons-defs.svg#icon-heart-transparent"></use></svg>
-//       </button>
-//     </div>
-//     <div class="card-news__info">
-//       <h3 class="card-news__title"> ${title}</h3>
-//       <p class="card-news__info-chort"> ${abstract}...</p>
-//       <div class="card-information">
-//         <div class="card-infrmation__data"> ${published_date}</div>
-//         <a class="card__infotion__more" rel="nofollow noindex noreferrer" target="_blank" href="${url}">Read more</a>
-//       </div>
-//     </div>
-//     <div class="owerlay-readed">
-//       <p class="owerlay-readed__info" aria-label="readed">Already read <svg class="owerlay-readed__icon" width="18"  height="18"><use href="../images/icons-defs.svg#icon-readed"></use></svg></p>
-//     </div>
-//   </li>`;
-//     })
-//     .join('');
 // }
