8bc5b95cea2a4a2265ea05d425adcc55422021d2
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-02-23 23:35:04 +0200

    add all functions

--- src/js/getPopoularProduct.js
+++ src/js/getPopoularProduct.js
@@ -1,9 +1,9 @@
 import { getPopularNewsAPI } from './api/news-api.js';
 import { getMarkupWeather } from './markups/weather-markup.js';
 import { weatherData } from './markups/weather-markup.js';
-import { valuePage, pagination } from './pagination.js';
+import { ref, valuePage, pagination } from './pagination.js';
 import { markup } from './markups/newsCard.js';
-import { checkFavCards } from "./addAndRemoveFromFavorite.js";
+import { checkFavCards } from './addAndRemoveFromFavorite.js';
 
 const popularNewsGallery = document.querySelector('.news-gallery');
 const notFoundPage = document.querySelector('.not-found');
@@ -13,7 +13,8 @@ async function getPopularProduct() {
     const getNews = await getPopularNewsAPI();
     // console.log('Arr objects with mostpopular News', getNews.results);
     const newsArr = getNews.results;
-
+    getAmountCards(newsArr);
+    console.log('valuePage', valuePage);
     let markupNews = '';
     const markupWeather = getMarkupWeather({ data: weatherData });
     // console.log(markupWeather);
@@ -28,8 +29,6 @@ async function getPopularProduct() {
         } else {
           markupNews += markup(newsArr[i]);
         }
-        valuePage.amountCards = 5;
-        valuePage.totalPages = Math.ceil(newsArr.length / valuePage.amountCards);
       }
     }
     if (window.innerWidth >= 768 && window.innerWidth < 1280) {
@@ -39,8 +38,6 @@ async function getPopularProduct() {
         } else {
           markupNews += markup(newsArr[i]);
         }
-        valuePage.amountCards = 8;
-        valuePage.totalPages = Math.ceil(newsArr.length / valuePage.amountCards);
       }
     }
     if (window.innerWidth >= 1280) {
@@ -50,8 +47,6 @@ async function getPopularProduct() {
         } else {
           markupNews += markup(newsArr[i]);
         }
-        valuePage.amountCards = 9;
-        valuePage.totalPages = Math.ceil(newsArr.length / valuePage.amountCards);
       }
     }
     popularNewsGallery.innerHTML = markupNews;
@@ -64,3 +59,131 @@ async function getPopularProduct() {
 }
 
 getPopularProduct();
+window.addEventListener('resize', getPopularProduct);
+
+function getAmountCards(array) {
+  if (window.innerWidth < 768) {
+    valuePage.amountCards = 5;
+    valuePage.totalPages = Math.ceil(array.length / valuePage.amountCards);
+  }
+
+  if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+    valuePage.amountCards = 8;
+    valuePage.totalPages = Math.ceil(array.length / valuePage.amountCards);
+  }
+
+  if (window.innerWidth >= 1280) {
+    valuePage.amountCards = 9;
+    valuePage.totalPages = Math.ceil(array.length / valuePage.amountCards);
+  }
+}
+
+let chunkNewsArr = [];
+
+ref.paginationEl.addEventListener('click', e => {
+  const ele = e.target;
+  // console.log(ele);
+
+  if (ele.dataset.page) {
+    const pageNumber = parseInt(e.target.dataset.page, 10);
+    valuePage.curPage = pageNumber;
+    console.log(pageNumber);
+  }
+  // console.log(valuePage.amountCards);
+
+  // getPopularProductData()
+  // .then(array => {
+  //   renderNewsMarkup(array, valuePage.amountCards);
+  // })
+  // .catch(error => console.error(error));
+  // renderNewsMarkup(newsArr, valuePage.amountCards);
+  // goToTop();
+});
+
+ref.paginationContainerEl.addEventListener('click', e => {
+  // handleButton(e.target);
+
+  // getPopularProductData()
+  // .then(array => {console.log(array)
+  //   renderNewsMarkup(array, valuePage.amountCards);
+  // })
+  // .catch(error => console.error(error));
+
+  // goToTop();
+});
+
+async function getPopularProductData() {
+  const getNews = await getPopularNewsAPI();
+  // console.log('Arr objects with mostpopular News', getNews.results);
+  const newsArr = await getNews.results;
+  getAmountCards(newsArr);
+  return newsArr;
+}
+
+function renderNewsMarkup(data, amountCards) {
+  chunkArray(data, amountCards);
+  // console.log('chunkArray', chunkArray(getNews.results, valuePage.amountCards));
+
+  for (let i = 0; i <= chunkNewsArr.length; i += 1) {
+    // console.log(chunkNewsArr[i]);
+    if (valuePage.curPage === i + 1) {
+      // console.log('page', chunkNewsArr[i]);
+      createMarkupWithChunkArray(chunkNewsArr[i]);
+      break;
+    }
+  }
+  pagination(valuePage);
+
+  // handleButtonLeft();
+  // handleButtonRight();
+}
+
+function chunkArray(arrayData, chunkSize) {
+  while (arrayData.length) {
+    chunkNewsArr.push(arrayData.splice(0, chunkSize));
+  }
+}
+
+function createMarkupWithChunkArray(array) {
+  let markupNews = '';
+  getAmountCards(newsArr);
+  const markupWeather = getMarkupWeather({ data: weatherData });
+  // console.log(markupWeather);
+  // console.log({ data: weatherData });
+  const itemWeather = `<li class="weather__card">${markupWeather.markup}</li>`;
+  // console.log(itemWeather);
+
+  if (window.innerWidth < 768) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (i === 0) {
+        markupNews += itemWeather;
+      } else {
+        markupNews += markup(array[i]);
+      }
+    }
+  }
+  if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (i === 1) {
+        markupNews += itemWeather;
+      } else {
+        markupNews += markup(array[i]);
+      }
+    }
+  }
+  if (window.innerWidth >= 1280) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (i === 2) {
+        markupNews += itemWeather;
+      } else {
+        markupNews += markup(array[i]);
+      }
+      if (array.length === 2) {
+        markupNews += itemWeather;
+      }
+    }
+  }
+  popularNewsGallery.innerHTML = markupNews;
+  checkFavCards();
+  // console.log(markupNews);
+}
